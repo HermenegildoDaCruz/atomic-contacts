ae1891a29fb8fef1a7c34928a9261717b159938b
Hermenegildo Hopson de Almeida da Cruz <138516502+HermenegildoDaCruz@users.noreply.github.com>
2025-12-05 16:08:13 +0100

    Removing strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.css'
 import App from './App.jsx'
 
+// <StrictMode></StrictMode>,
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
